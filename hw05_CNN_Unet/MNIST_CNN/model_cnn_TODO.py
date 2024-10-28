74576def1a901ec84d5713d3df949ac08bf7bf15
Rainbow-prince <3491810242@qq.com>
2024-10-28 18:53:50 +0800

    初步读懂代码，完成了Q1的setp3：构建了一个两层的CNN神经网络，并成功运行

--- hw05_CNN_Unet/MNIST_CNN/model_cnn_TODO.py
+++ hw05_CNN_Unet/MNIST_CNN/model_cnn_TODO.py
@@ -6,29 +6,38 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.pool1 = nn.MaxPool2d(2)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        self.pool1 = nn.MaxPool2d(2, 2)
 
-        # TODO: uncomment this, choose proper layers, modify their parameters slightly if necessarily
-        if 1 == 2:
-            self.conv2 = nn.Conv2d(32, 64, 3, 1)
-            self.dropout1 = nn.Dropout(0.3)
-            self.dropout2 = nn.Dropout(0.3)
-            self.fc1 = nn.Linear(9216, 128)
-            self.fc2 = nn.Linear(128, 10)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.pool2 = nn.MaxPool2d(2, 2)
 
-        # TODO: remove this dummy fc layer, use fc1 and/or fc2 as output
-        self.fc_dummy = nn.Linear(5408, 10)
+        self.dropout1 = nn.Dropout(0.3)
+
+        # 3136 == 64 * 7 * 7 == 1/2 * 1/2 * (64 * 28 * 28)
+        self.fc1 = nn.Linear(3136, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
+        # 第一层卷积 relu激活 池化
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
 
-        # TODO: remove the following dummy lines
-        # change to your layers forward function
-        # make sure the output is [Batch_size, 10] as there are 10 classes to predict
+        # 第二层卷积 relu激活 池化
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+
+        # 展平特征图
         x = torch.flatten(x, 1)
-        out = self.fc_dummy(x)
+
+        # dropout
+        x = self.dropout1(x)
+
+        # 全连接层
+        x = F.relu(self.fc1(x))
+        out = self.fc2(x)
+
+        # 确保输出形状为 [Batch_sieze, 10]
         # print(out.size())
         assert out.ndim == 2 and out.size(0) == x.size(0) and out.size(1) == 10
 
