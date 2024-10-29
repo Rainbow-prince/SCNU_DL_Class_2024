d5f3628ffc9655a95ca31264c9847b5e5a43d825
Rainbow-prince <3491810242@qq.com>
2024-10-29 14:30:03 +0800

    完成bonus，注意原始图片的路径即可

--- hw05_CNN_Unet/Diffusion_UNET/test_unet.py
+++ hw05_CNN_Unet/Diffusion_UNET/test_unet.py
@@ -1,13 +1,16 @@
 from PIL import Image
 import torch
 from diffusers import StableDiffusionImg2ImgPipeline
+from diffusers import DiffusionPipeline
 
 # Image-to-Image text-guided generation with Stable Diffusion
 # Download model https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/tree/main
 # Use a 3090/4090/A100 to perform the following experiments
 
-model_id_or_path = "/data/LLM_MODEL/stabilityai/stable-diffusion-v1-4"
-pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path, torch_dtype=torch.float16)
+# model_id_or_path = "/data/LLM_MODEL/stabilityai/stable-diffusion-v1-4"
+# pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path, torch_dtype=torch.float16)
+
+pipe = DiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5")
 pipe = pipe.to("cuda")
 
 # check how many CrossAttnDownBlock2D/CrossAttnUpBlock2D blobks in the unet.
